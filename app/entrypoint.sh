8c64b8a969104299f84dfd475e861a10a69905cf
root <root@ip-172-31-8-220.ap-south-1.compute.internal>
2021-03-04 20:09:46 +0000

    djoser not working

---
+++ app/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+pipenv run python manage.py makemigrations 
+
+pipenv run python manage.py migrate 
+
+pipenv run python manage.py collectstatic 
+
+pipenv run gunicorn hello.wsgi:application --bind 0.0.0.0:8000
+
